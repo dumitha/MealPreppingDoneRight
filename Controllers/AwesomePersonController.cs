82128343df288d7efb388a5a897313334e0e7178
Carrie Dumit <hdcarrie22@gmail.com>
2018-09-06 19:56:52 -0500

    first controllers

---
+++ Controllers/AwesomePersonController.cs
@@ -0,0 +1,95 @@
+﻿using MealPreppingDoneRight.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Web.Mvc;
+
+namespace MealPreppingDoneRight.Controllers
+{
+    public class AwesomePersonController : Controller
+    {
+        // GET: AwesomePersonUsers
+        public ActionResult AwesomePersonIndex()
+        {
+           var  awesomePerson = new AwesomePerson();
+
+            awesomePerson.AweesomeName = "Carrie";
+            
+            return View("AwesomePersonIndex", awesomePerson);
+        }
+
+        // GET: AwesomePersonUsers/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AwesomePersonUsers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AwesomePersonUsers/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AwesomePersonUsers/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AwesomePersonUsers/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AwesomePersonUsers/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AwesomePersonUsers/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Controllers/HealthyMealController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MealPreppingDoneRight.Controllers
+{
+    public class HealthyMealController : Controller
+    {
+        // GET: HealthyMeal
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: HealthyMeal/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: HealthyMeal/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: HealthyMeal/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HealthyMeal/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: HealthyMeal/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HealthyMeal/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: HealthyMeal/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
