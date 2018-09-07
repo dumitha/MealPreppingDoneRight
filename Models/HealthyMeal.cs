3014eaccdcdc56009e5c5a7825e700de810d7563
Carrie Dumit <hdcarrie22@gmail.com>
2018-09-06 19:13:15 -0500

    First Models

---
+++ Models/HealthyMeal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MealPreppingDoneRight.Models
+{
+    public class HealthyMeal
+    {
+        public int HealthyMealID { get; set; }
+        public string HealthyMealName { get; set; }
+        public string HealthyMealDescription { get; set; }
+        public enum HealthyMealCategories
+        {
+            Vegetarian,
+            Vegan,
+            Pescetarian,
+            Paleo,
+            Keto,
+            DiabeticDiet
+        }
+    }
+}
---
+++ Models/YummyFood.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MealPreppingDoneRight.Models
+{
+    public class YummyFood
+    {
+        public string YummyFoodName { get; set; }
+        public string YummyFoodDescription { get; set; }
+        public int YummyFoodNameID { get; set; }
+        public enum YummyFoodCategory {
+            Vegetables,
+            Protein,
+            PlantProtein,
+            Dairy,
+            Carbs,
+            Fruits,
+            Grains
+        }
+
+        
+    }
+}
